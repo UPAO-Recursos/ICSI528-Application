3ae711f9c1a0baff767a46ddb4beda8bfec64200
Walter Leturia <leturia@live.com>
2024-06-20 20:15:36 -0500

    frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-tdd';
+  libros: any = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.GetLibros();
+  }
+
+  public GetLibros() {
+    this.http.get(`${environment.BASE_URL}/libros`).subscribe(data => this.libros = data);
+  }
 }
